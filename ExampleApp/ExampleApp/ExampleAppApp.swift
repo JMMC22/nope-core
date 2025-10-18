c401fab09c51b1e7ef8b8a46c8a45e5d1719442b
José María Márquez <jmmarquezcrespo@gmail.com>
2025-10-18 13:58:11 +0200

    chore: initial project

---
+++ ExampleApp/ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Jose Maria Marquez on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

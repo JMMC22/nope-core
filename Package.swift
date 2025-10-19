0239fd9f0781f4378a69851a88cb776c47fb4f9a
José María Márquez <jmmarquezcrespo@gmail.com>
2025-10-19 09:33:00 +0200

    refactor: update packages directories and targets

--- Package.swift
+++ Package.swift
@@ -13,19 +13,46 @@ let package = Package(
             targets: ["nope-core"]
         ),
     ],
-    dependencies: [
-        .package(path: "./nope-analytics"),
-        .package(path: "./nope-notifications"),
-        .package(path: "./nope-persistence"),
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "nope-core",
             dependencies: [
-                .product(name: "nope-analytics", package: "nope-analytics"),
-                .product(name: "nope-notifications", package: "nope-notifications"),
-                .product(name: "nope-persistence", package: "nope-persistence"),
-            ]
+                "nope-analytics",
+                "nope-notifications",
+                "nope-persistence",
+            ],
+            path: "Sources"
+        ),
+        .target(
+            name: "nope-analytics",
+            dependencies: [],
+            path: "nope-analytics/Sources"
+        ),
+        .testTarget(
+            name: "nope-analytics-tests",
+            dependencies: ["nope-analytics"],
+            path: "nope-analytics/Tests"
+        ),
+        .target(
+            name: "nope-notifications",
+            dependencies: [],
+            path: "nope-notifications/Sources"
+        ),
+        .testTarget(
+            name: "nope-notifications-tests",
+            dependencies: ["nope-notifications"],
+            path: "nope-notifications/Tests"
+        ),
+        .target(
+            name: "nope-persistence",
+            dependencies: [],
+            path: "nope-persistence/Sources"
+        ),
+        .testTarget(
+            name: "nope-persistence-tests",
+            dependencies: ["nope-persistence"],
+            path: "nope-persistence/Tests"
         ),
     ]
 )
